69a275de2b09bfb8007d85505aaa32b4c5a84f90
Hubert Vecchioli <hvecchio@made-f0Dr10s8.clusters.42paris.fr>
2024-06-24 18:03:49 +0200

    Pre-work

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hvecchio <hvecchio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 16:41:33 by hvecchio          #+#    #+#             */
-/*   Updated: 2024/06/24 17:42:51 by hvecchio         ###   ########.fr       */
+/*   Updated: 2024/06/24 17:58:31 by hvecchio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int ac, char **av)
 	if (ac < 5 || ac > 6)
 		return  ft_exit('i');
 	ft_parse(ac, av, &philo_pack);
-	// ft_planning() // case 1 philo
-	// ft_orchestrate()
+	ft_init_philos(&philo_pack);
+	
 	return (ft_free(&philo_pack), 0);
 }
